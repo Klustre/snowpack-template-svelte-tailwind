503264aad077d4879756516d39dd17dd9aab5a6d
Klustre <subs@klustre.nl>
2020-12-04 13:58:53 +0100

    Adds Snowpack Svelte Tailwind setup

---
+++ snowpack.config.js
@@ -0,0 +1,18 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-postcss',
+  ],
+  install: [],
+  installOptions: {},
+  devOptions: {},
+  buildOptions: {},
+  proxy: {},
+  alias: {},
+};
---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import {onMount} from 'svelte';
+
+  let count = 0;
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div class="text-center">
+  <header class="text-2xl bg-white text-gray-600 flex flex-col items-center justify-center min-h-screen">
+    <img src="/logo.svg" class="h-96 pointer-events-none mb-12 animate-pulse" alt="logo" />
+    <p>Edit <code>src/App.svelte</code> and save to reload.</p>
+    <p>Page has been open for <code>{count}</code> seconds.</p>
+    <p>
+      <a class="text-red-500" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+        Learn Svelte
+      </a>
+    </p>
+  </header>
+</div>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+  
+  :global(body) {
+    @apply m-0 font-sans;
+  }
+
+  code {
+    @apply bg-gray-900 px-2 py-1 rounded;
+  }
+  
+  p {
+    @apply m-1.5;
+  }
+</style>
